d7190199bdd7c20a94ad46eb921d1ba5d6c2331f
Darshit Garg <gargdarshit231@gmail.com>
2026-04-25 23:02:36 +0530

    Upgraded the version of dependencies, java, javafx, maven to latest

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.minesweeper {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires java.base;
 
     opens com.mycompany.minesweeper to javafx.fxml;
